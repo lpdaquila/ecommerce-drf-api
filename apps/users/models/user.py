8eab875d2eb9702b07e95d57500a98c3d9f525c9
lpdaquila <99849531+lpdaquila@users.noreply.github.com>
2025-05-26 18:26:46 -0300

    updated 'User' models and added 'Groups' for users

--- apps/users/models/user.py
+++ apps/users/models/user.py
@@ -10,3 +10,29 @@ class User(AbstractBaseUser):
     
     def __str__(self) -> str:
         return self.email
+    
+class Group(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+class GroupPermissions(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    permission = models.ForeignKey(Permission, on_delete=models.CASCADE)
+    
+    class Meta:
+        unique_together = ('group', 'permission')
+    
+    def __str__(self) -> str:
+        return f"{self.group.name} - {self.permission.name}"
+    
+class UserGroups(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    
+    class Meta:
+        unique_together = ('user', 'group')
+    
+    def __str__(self) -> str:
+        return f"{self.user.email} - {self.group.name}"
