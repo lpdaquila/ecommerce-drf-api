f98bf6375cac91bc9f51e6815f33c9c71582018b
lpdaquila <99849531+lpdaquila@users.noreply.github.com>
2025-06-14 23:31:43 -0300

    Fix: User model cannot create superuser, and admin panel configurated

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,9 +1,60 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models.profile import Profile
 from .models.address import Address
+from .models.users import User
 
-admin.site.register(Profile)
-admin.site.register(Address)
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = (
+        "email",
+        "document",
+        "is_staff",
+        "is_superuser",
+    )
+    list_filter = (
+        "is_staff",
+        "is_superuser",
+        "is_active",
+    )
+    ordering = ("email",)
+    search_fields = ("email", "document",)
+    
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal Info", {"fields": ("document",)}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
+        ("Dates", {"fields": ("last_login",)}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "document", "password1", "password2", "is_staff", "is_active",)
+        }),
+    )
+    
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "name", "email", "phone",)
+    search_fields = ("name", "email", "phone",)
+    list_filter = ("email",)
+    
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = (
+        "profile",
+        "address",
+        "number",
+        "complement",
+        "district",
+        "zip_code",
+        "city",
+        "state",
+    )
+    search_fields = ("city", "state", "zip_code")
+    list_filter = ("state", "city")
 
 
--- apps/users/models/users.py
+++ apps/users/models/users.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import (
     BaseUserManager, Permission)
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError("Email is required.")
         email = self.normalize_email(email)
@@ -13,12 +13,12 @@ class UserManager(BaseUserManager):
         user.save()
         return user
     
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         return self.create_user(email, password, **extra_fields)
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     """
     User
         name - Char
@@ -35,6 +35,8 @@ class User(AbstractBaseUser):
     date_joined = models.DateTimeField(auto_now_add=True, null=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    
+    objects = UserManager()
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name', 'document']
